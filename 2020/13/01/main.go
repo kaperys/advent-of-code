e92dcbd26e12238b8344b1c89d35d2d2d77cdbeb
Mike Kaperys <mike@kaperys.io>
2020-12-14 09:42:18 +0000

    day 13 part 01

---
+++ 2020/13/01/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := ioutil.ReadFile("2020/13/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	in := strings.Split(strings.TrimSpace(string(input)), "\n")
+	timestamp, _ := strconv.Atoi(in[0])
+	busses := strings.Split(in[1], ",")
+
+	var (
+		earliestBus int
+		earliestDel int = math.MaxInt64
+	)
+
+	for _, bus := range busses {
+		if bus == "x" {
+			continue
+		}
+
+		ts, _ := strconv.Atoi(bus)
+		earliest := ((timestamp / ts) * ts) + ts
+
+		if delta := (timestamp - earliest) * -1; delta < earliestDel {
+			earliestDel = delta
+			earliestBus = ts
+		}
+	}
+
+	println("I need to wait", earliestBus*earliestDel, "to take bus", earliestBus)
+}
