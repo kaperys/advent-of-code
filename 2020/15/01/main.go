e7bfd96ca094cc223f07c20779483251673051f0
Mike Kaperys <mike@kaperys.io>
2020-12-15 14:18:50 +0000

    day 15

---
+++ 2020/15/01/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+const target = 2020
+
+// https://adventofcode.com/2020/day/15
+func main() {
+	input, err := ioutil.ReadFile("2020/15/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	var numbers []int
+
+	for _, i := range strings.Split(strings.TrimSpace(string(input)), ",") {
+		i, err := strconv.Atoi(i)
+		if err != nil {
+			panic(err)
+		}
+
+		numbers = append(numbers, i)
+	}
+
+	var (
+		last   int
+		memory = make(map[int]int)
+	)
+
+	for i := 0; i < target; i++ {
+		if i < len(numbers) {
+			memory[numbers[i]] = i + 1
+			last = numbers[i]
+			continue
+		}
+
+		l, ok := memory[last]
+		if ok {
+			memory[last] = i
+			last = i - l
+			continue
+		}
+
+		memory[last] = i
+		last = 0
+	}
+
+	println("the last number spoken was", last)
+}
