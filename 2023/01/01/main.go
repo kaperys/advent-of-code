1bb5b17801af8d39bd6f0fa5cbc7ae4b3147be01
Mike Kaperys <mike@kaperys.io>
2023-12-01 16:53:14 +0000

    2023 day 1

---
+++ 2023/01/01/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var nums = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
+// It looks like some of the digits are actually spelled out with letters: one, two,
+// three, four, five, six, seven, eight, and nine also count as valid "digits"
+func main() {
+	input, err := os.Open("2023/01/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer input.Close()
+
+	var total int64
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		var (
+			li, ri int
+			l, r   string
+		)
+
+		ri = len(text) - 1
+		for {
+			for _, v := range nums {
+				if string(text[li]) == v && l == "" {
+					l = v
+				}
+
+				if string(text[ri]) == v && r == "" {
+					r = v
+				}
+			}
+
+			if l != "" && r != "" {
+				num, _ := strconv.ParseInt(l+r, 10, 64)
+				total += num
+				break
+			}
+
+			li++
+			ri--
+		}
+	}
+
+	println(total)
+}
---
+++ 2023/01/02/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var nums = map[string]string{
+	"1": "one",
+	"2": "two",
+	"3": "three",
+	"4": "four",
+	"5": "five",
+	"6": "six",
+	"7": "seven",
+	"8": "eight",
+	"9": "nine",
+}
+
+// It looks like some of the digits are actually spelled out with letters: one, two,
+// three, four, five, six, seven, eight, and nine also count as valid "digits"
+func main() {
+	input, err := os.Open("2023/01/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer input.Close()
+
+	var total int64
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		var li, ri int
+		ri = len(text) - 1
+		var lv, rv, l, r string
+
+		for {
+			lv += string(text[li])
+			rv = string(text[ri]) + rv
+
+			for k, v := range nums {
+				// v = "-" // disable part 2
+				if (strings.HasSuffix(lv, v) || strings.HasSuffix(lv, k)) && l == "" {
+					l = k
+				}
+
+				if (strings.HasPrefix(rv, v) || strings.HasPrefix(rv, k)) && r == "" {
+					r = k
+				}
+			}
+
+			if l != "" && r != "" {
+				// print(l, r)
+				num, _ := strconv.ParseInt(l+r, 10, 64)
+				total += num
+				break
+			}
+
+			li++
+			ri--
+		}
+	}
+
+	println(total)
+}
